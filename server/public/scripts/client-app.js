dab2c39c194eeb6ed5b5b4bf25e79e1e57e8bc19
Andrew Froiland <afroiland@Andrews-MacBook-Pro-2.local>
2016-11-18 19:01:04 -0600

    got some functions working

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -1,8 +1,35 @@
 $(document).ready(function () {
   console.log('test');
-
-
-
+//    getTasks();
+    $('#taskSubmit').on('click', postTask);
 
 
 });
+
+function getTasks() {
+  $.ajax({
+    type: 'GET',
+    url: '/tasks',
+    success: function(tasks) {
+      appendTasks(tasks);
+    },
+    error: function() {
+      console.log('Database error');
+    }
+
+  })
+}
+
+function appendTasks() {
+  $("#container").empty();
+  for (var i = 0; i < tasks.length; i++) {
+    $el = $('#container').children().last();
+    var task = tasks[i];
+    $el.data('id', task.id);
+    $el.append(task);
+  }
+}
+
+function postTask() {
+  console.log("postTask test");
+}
