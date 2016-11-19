821cc2e5368e92bd33f0e1737dc748606f9b4bb6
Andrew Froiland <afroiland@Andrews-MacBook-Pro-2.local>
2016-11-19 14:06:59 -0600

    post working, db updating

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   console.log('test');
-//    getTasks();
+    getTasks();
     $('#taskSubmit').on('click', postTask);
 
 
@@ -20,16 +20,38 @@ function getTasks() {
   })
 }
 
-function appendTasks() {
-  $("#container").empty();
+function appendTasks(tasks) {
+  $("#containerSpan").empty();
   for (var i = 0; i < tasks.length; i++) {
     $el = $('#container').children().last();
     var task = tasks[i];
     $el.data('id', task.id);
-    $el.append(task);
+//    $el.append(task);
+    console.log('append task: ', task);
+    var string = '<p>Task: '+tasks[i].taskname+' Status:'+tasks[i].completionstatus+'</p>'
+    console.log('append: ', string);
+    $el.append(string);
   }
 }
 
 function postTask() {
+  event.preventDefault();
   console.log("postTask test");
+  var task = {};
+  $.each($('#taskEntry').serializeArray(), function (i, field) {
+    task[field.name] = field.value;
+  });
+  console.log('post task: ', task);
+
+  $.ajax({
+    type: 'POST',
+    url: '/tasks',
+    data: task,
+    success: function(response) {
+      getTasks();
+    },
+    error: function() {
+      console.log('could not post a new task');
+    }
+  })
 }
